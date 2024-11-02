1eb0776e9f27dc73222fee599ec155f3a73670a5
Lalramnunmawia <80774787+papuia13@users.noreply.github.com>
2024-11-03 03:56:44 +0530

    installing NPM vite and initilizing with chakra ui

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+
+import { Button } from "@chakra-ui/react/button"
+
+function App() {
+
+  return (
+    <>
+      <Button>
+        Hello World
+      </Button>
+    </>
+  )
+}
+
+export default App
